9c806058681b3334aca739eece2ba25d9c2fcd54
Arnav Dandekar <146211212+4rnv@users.noreply.github.com>
2024-08-25 01:56:49 +0530

    Demo code provided by BFL

---
+++ flux1.py
@@ -0,0 +1,20 @@
+import time
+import torch
+from diffusers import FluxPipeline
+
+pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe.enable_model_cpu_offload() #No GPU
+
+prompt = "A cat holding a sign that says hello world"
+seed = 42
+image = pipe(
+    prompt,
+    output_type="pil",
+    num_inference_steps=4, #use a larger number if you are using [dev]
+    generator=torch.Generator("cpu").manual_seed(seed)
+).images[0]
+if(image):
+    image.save(f"{int(time.time()*1000000)}.png")
+    print("Image saved successfully")
+else:
+    print("Error saving image")
